f7d7e7ab63b90f5edc858d4ddea2a7a04684d4ec
Alex <alexyoup@outlook.com>
2023-09-19 22:51:01 +0300

    init: new code for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var NumReverse = parseInt(Math.abs(n).toString().split('').reverse().join(''));
+    return NumReverse;
 }
